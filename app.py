50704846ba3d038a84fb27168f1a883e0e15b212
Sraman <sramakoo@gmail.com>
2018-11-14 17:19:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
+def source():
+ html = 'Hello World!'
+ return html
