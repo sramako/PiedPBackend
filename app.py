86a01a2eb7d33215f11a1cc032506ddfb88b49c3
Sraman <sramakoo@gmail.com>
2018-11-14 21:36:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,6 +31,6 @@ def restaurants():
 	return ' '.join(ret)
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=port)
 	port = int(os.environ.get('PORT', 5000))
+	app.run(host='0.0.0.0', port=port)
 	# app.run()
